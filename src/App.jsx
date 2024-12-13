a5da5d16d100cf013d09e352d1a1e4617884176c
Vu Duy Hai <duyhai121120@gmail.com>
2024-12-13 19:44:04 +0900

    Add Base Gemini API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./components/SideBar";
+import Gemini from "./gemini/index.js";
 
 function App() {
+
+    useEffect(() => {
+
+    })
     return (
         <>
             <div className="bg-primaryBg-default h-screen flex">
